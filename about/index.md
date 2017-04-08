631295a13e18ae4652472a8c1ec95671c6e70136
Caio Santana Jacobina <caio@ua.pt>
2017-04-08 21:49:33 +0100

    page "about" -> done.

--- about/index.md
+++ about/index.md
@@ -5,3 +5,20 @@ tags: [about, glua]
 date: 2016-03-21
 comments: false
 ---
+
+# <center>Sobre nós</center>
+
+O **GLUA**, *Grupo Linux da Universidade de Aveiro*, é um grupo integrado no Departamento de Eletrónica, Telecomunicações e Informática (**DETI**) que tem como objetivo divulgar e promover o **Software Livre e Aberto** (F/OSS), mais nomeadamente soluções baseadas em sistemas GNU/Linux.
+Parte das nossas ações traduzem-se em eventos organizados na **Universidade de Aveiro**, que visam na sua maioria, em **divulgar e ensinar** ferramentas livres à comunidade académica.
+Os membros ativos do GLUA são maioritariamente alunos do DETI, embora o grupo esteja aberto a todos os que queiram contribuir, seja de que maneira for.
+
+## Sede
+
+O GLUA está sediado na sala **120 do DETI**.
+
+## Hora de atendimento
+
+O horário de atendimento do GLUA encontra-se **encerrado por tempo indeterminado**.
+
+## Junta-te a nós!
+Se pretende colaborar com o GLUA de alguma maneira, contacte-nos via *e-mail* < ___glua@ua.pt___ >
---
+++ wp_html/wp-config.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'wordpress');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'wordpress');
+
+/** MySQL hostname */
+define('DB_HOST', 'db:3306');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'f8403d39921ce21ec1442d0d5c10ec87c9f66e6a');
+define('SECURE_AUTH_KEY',  'd1dce92d56abcbf20aff461f13737c712ea65368');
+define('LOGGED_IN_KEY',    '78d25ce4c32a83f619fb9b4aa56a78ae077d9d26');
+define('NONCE_KEY',        'b2d833fa76a4b98f104f713aa8b85173cff17326');
+define('AUTH_SALT',        '53f8e71ca9ed91839f9b54cc1cd4927b80d26172');
+define('SECURE_AUTH_SALT', '223fb2301a2beb5a26b8cb189ad09b797e899633');
+define('LOGGED_IN_SALT',   '264424b564673c64f18e672a8c7976031cc13e96');
+define('NONCE_SALT',       'e4172ceb0bf7c732100f53ab870890be7a83bf45');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
