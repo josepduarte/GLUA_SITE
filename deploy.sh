854cfd91eaaa59baf5e0dd67f42df86d6bc3fc41
José Pedro Duarte <josepduarte66@gmail.com>
2017-04-21 13:19:52 +0100

    Cleaned up deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,38 +1,15 @@
 #!/bin/bash
 
-cleanup()
-{
-  echo -e "\x1B[31m \n\n Do you really wanna deploy or not?? baaaahhhhh...Cleaning up for you... \x1B[0m"
-  sed -i -e 's/https:\/\/glua.ua.pt/http:\/\/localhost:4000/g' _config.yml
-  bundle exec jekyll clean
-  echo -e "\x1B[32m Cleaned your mess. You're welcome. \x1B[0m"
-  exit $?
-}
-
-# catch ctrl+c and cleanup
-trap cleanup SIGINT
-
-main()
-{
-  echo -e "\x1B[33mChanging config.yml properly \x1B[0m"
-  sed -i -e 's/http:\/\/localhost:4000/https:\/\/glua.ua.pt/g' _config.yml
-  echo -e "\x1B[32mDone with config.yml! \x1B[0m"
-  echo -e "\x1B[33mBuilding site. \x1B[0m"
-  bundle exec jekyll build
-  echo -e "\x1B[32mDone with building! \x1B[0m"
-  echo -e "\x1B[33mDeploying to glua.ua.pt\x1B[0m"
-  echo -e "\x1B[35mEnter your username and press [ENTER]:\x1B[0m "
-  read username
-  rsync -crvz --rsh='ssh' --delete-after --delete-excluded   ./_site/ "$username"@glua.ua.pt:/var/www/glua.ua.pt/
-  if [ $? -ne 0 ]; then
-      echo -e "\x1B[31m \n\n Error trying to connect the server (rsync) \x1B[0m"
-      sed -i -e 's/https:\/\/glua.ua.pt/http:\/\/localhost:4000/g' _config.yml
-      exit 1
-  fi
-  echo -e "\x1B[32mDone with deploy!\x1B[0m"
-  echo -e "\x1B[33mRetrieve config.yml \x1B[0m"
-  sed -i -e 's/https:\/\/glua.ua.pt/http:\/\/localhost:4000/g' _config.yml
-  echo -e "\x1B[32mDone with everything! \x1B[0m"
-}
-
-main
+echo -e "\x1B[33mBuilding site. \x1B[0m"
+bundle exec jekyll build
+echo -e "\x1B[32mDone with building! \x1B[0m"
+echo -e "\x1B[33mDeploying to glua.ua.pt\x1B[0m"
+echo -e "\x1B[35mEnter your username and press [ENTER]:\x1B[0m "
+read username
+rsync -crvz --rsh='ssh' --delete-after --delete-excluded   ./_site/ "$username"@glua.ua.pt:/var/www/glua.ua.pt/
+if [ $? -ne 0 ]; then
+    echo -e "\x1B[31m \n\n Error trying to connect the server (rsync) \x1B[0m"
+    sed -i -e 's/https:\/\/glua.ua.pt/http:\/\/localhost:4000/g' _config.yml
+    exit 1
+fi
+echo -e "\x1B[32mDone with deploy!\x1B[0m"
